68889b01c9e8495897a03dcf749b1e3cbf9ad9e1
Arturas-B <arty2004@gmail.com>
2019-03-25 23:31:08 +0200

    Pridedu PHP01 , 02 ir 03 uzduotis

---
+++ php2/index02.php
@@ -0,0 +1,12 @@
+
+
+<?php
+
+    $salis ='Lietuva';
+    $miestas ='Vilnius';
+    $populiacija = 574221;
+    ?>
+      <?php echo  $miestas.' - Lietuvos sostine ' ; ?>
+      <?php echo  "$miestas ir $salis sparciai auga" ; ?>
+
+
